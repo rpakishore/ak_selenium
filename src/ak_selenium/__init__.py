be6289ceb14d6c1bfa81d93740f1718a4d83c148
Arun Kishore <50844460+rpakishore@users.noreply.github.com>
2023-06-21 16:50:05 -0700

    Update __init__.py

--- src/ak_selenium/__init__.py
+++ src/ak_selenium/__init__.py
@@ -1,4 +1,4 @@
 """Selenium package with requests integration and anti-bot detection measures"""
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
-from ak_selenium.browser import Chrome
+from ak_selenium.browser import Chrome, By, Keys
