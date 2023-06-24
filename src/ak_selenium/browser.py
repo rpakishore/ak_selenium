0d8ea8fcec74ac302beb5a04bc17729db06827d0
Arun Kishore <50844460+rpakishore@users.noreply.github.com>
2023-06-24 00:06:43 -0700

    refactored

--- src/ak_selenium/browser.py
+++ src/ak_selenium/browser.py
@@ -7,16 +7,10 @@ from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-
-
 from selenium.webdriver.remote.webelement import WebElement
-
-
 from webdriver_manager.chrome import ChromeDriverManager
-
 from pathlib import Path
 import sys
-
 from typing import Literal
 
 class Chrome:
@@ -28,21 +22,45 @@ class Chrome:
 
     def __init__(
             self, headless:bool = False, 
-            Chrome_userdata_path:str=None, 
-            half_screen:bool=True):
+            chrome_userdata_path:str=None, 
+            half_screen:bool=True
+            ):
+        
         self.headless = headless
         
-        if not Chrome_userdata_path and sys.platform=="win32":
-            Chrome_userdata_path = Path.home() / 'AppData' / 'Local' / 'Google' / 'Chrome' / 'User Data'
-            if not Chrome_userdata_path.exists():
-                Chrome_userdata_path = None
+        if not chrome_userdata_path and sys.platform=="win32":
+            chrome_userdata_path = Path.home() / 'AppData' / 'Local' / 'Google' / 'Chrome' / 'User Data'
+            if not chrome_userdata_path.exists():
+                chrome_userdata_path = None
             else:
-                Chrome_userdata_path = str(Chrome_userdata_path)
-        self.Chrome_userdata_path = Chrome_userdata_path
+                chrome_userdata_path = str(chrome_userdata_path)
+        self.chrome_userdata_path = chrome_userdata_path
         self.half_screen = half_screen
         self.s = None
+        self.driver = None
         return None
-
+    
+    def __str__(self) -> str:
+        return f"""
+        Chrome.Object
+        UserAgent:{self.USERAGENT}
+        Implicit Wait Time: {self.IMPLICITLY_WAIT_TIME:.2f}s
+        Max Wait Time: {self.MAX_WAIT_TIME:.2f}s
+        Headless: {self.headless}
+        Chrome Userdata Path: {self.chrome_userdata_path}
+        Half Screen View: {self.half_screen}
+        """
+    
+    def __repr__(self) -> str:
+        return f"Chrome(headless={self.headless},\
+                chrome_userdata_path={self.chrome_userdata_path},\
+                half_screen={self.half_screen})"
+    
+    def __del__(self) -> None:
+        if self.driver:
+            self.driver.quit()
+        return None
+    
     def init_chrome(self) -> webdriver.Chrome:
         """
         Initializes a Chrome web driver with specific options and configurations.
@@ -57,15 +75,14 @@ class Chrome:
             options.add_argument("--window-size=1920,1080")
         options.add_argument("start-maximized")
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
-        if self.Chrome_userdata_path:
-            options.add_argument('--user-data-dir=' + self.Chrome_userdata_path)
+        if self.chrome_userdata_path:
+            options.add_argument('--user-data-dir=' + self.chrome_userdata_path)
         options.add_experimental_option('useAutomationExtension', False)
     
-        options = _ram_optimization_browser_options(options)
+        options = self._ram_optimization_browser_options(options)
 
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
                                        options=options)
-        driver = self.driver
 
         driver.implicitly_wait(self.IMPLICITLY_WAIT_TIME) 
 
@@ -89,35 +106,35 @@ class Chrome:
             size = driver.get_window_size()
             driver.set_window_size(size['width']/2, size['height'])
             driver.set_window_position(size['width']/2-13, 0)
+            
         self.driver = driver
+        
         return driver
 
-    def __str__(self) -> str:
-        return f"""
-        Chrome.Object
-        UserAgent:{self.USERAGENT}
-        Implicit Wait Time: {self.IMPLICITLY_WAIT_TIME:.2f}s
-        Max Wait Time: {self.MAX_WAIT_TIME:.2f}s
-        Headless: {self.headless}
-        Chrome Userdata Path: {self.Chrome_userdata_path}
-        Half Screen View: {self.half_screen}
-        """
-    
-    def __repr__(self) -> str:
-        return f"Chrome(headless={self.headless},\
-                Chrome_userdata_path={self.Chrome_userdata_path},\
-                half_screen={self.half_screen})"
-    
-    def __del__(self) -> None:
-        driver = self.driver
-        try:
-            driver.quit()
-        except Exception as e:
-            print(str(e))
+    @staticmethod
+    def _ram_optimization_browser_options(options: Options) -> Options:
+        options.add_argument("disable-infobars")
+        options.add_experimental_option("excludeSwitches", 
+                                        ['enable-automation', "enable-logging"])
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-application-cache")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--lang=en-US")
         
-        return None
-
+        # Based on https://stackoverflow.com/questions/59514049/unable-to-sign-into-google-with-selenium-automation-because-of-this-browser-or
+        options.add_argument("--allow-running-insecure-content")
+        options.add_argument("--disable-web-security")
+        return options
+    
     def Wait_for_locator(self,locator: tuple) -> None:
+        """
+        Wait until the element with the specified locator is present.
+
+        Args:
+            locator (tuple): The locator tuple (By, value) of the element to wait for.
+        """
         (
             WebDriverWait(self.driver, self.MAX_WAIT_TIME, 0.5)
             .until(EC.presence_of_element_located(locator))
@@ -125,13 +142,18 @@ class Chrome:
         return None
 
     @staticmethod
-    def fill_userinput_form(element, text, clear_existing=True) -> None:
+    def fill_userinput_form(element: WebElement, text: str, clear_existing: bool=True) -> None:
+        """
+        Fill a user input form element with the specified text.
+
+        Args:
+            element (WebElement): The input form element.
+            text (str): The text to fill in the form element.
+            clear_existing (bool): Whether to clear existing text before filling. Defaults to True.
+        """
         if clear_existing:
             element.clear()
         element.send_keys(text)
-        
-    def get_By_and_Keys(self):
-        return By, Keys
     
     def init_requests(self):
         import requests
@@ -184,20 +206,3 @@ class Chrome:
                 self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
     
-
-def _ram_optimization_browser_options(options: Options) -> Options:
-    options.add_argument("disable-infobars")
-    options.add_experimental_option("excludeSwitches", 
-                                    ['enable-automation', "enable-logging"])
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-application-cache")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--lang=en-US")
-    
-    # Based on https://stackoverflow.com/questions/59514049/unable-to-sign-into-google-with-selenium-automation-because-of-this-browser-or
-    options.add_argument("--allow-running-insecure-content")
-    options.add_argument("--disable-web-security")
-    return options
