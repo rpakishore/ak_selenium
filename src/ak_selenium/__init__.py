06029145d9e3dd245becd688ad90b33864d788e9
Arun Kishore <50844460+rpakishore@users.noreply.github.com>
2023-12-04 22:17:42 -0800

    Added Firefox Integration

--- src/ak_selenium/__init__.py
+++ src/ak_selenium/__init__.py
@@ -5,4 +5,5 @@ from ak_selenium.browser import RequestsSession
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from ak_selenium.chrome import Chrome
+from ak_selenium.firefox import Firefox
 from ak_selenium.helium_attribs import Element, Actions
