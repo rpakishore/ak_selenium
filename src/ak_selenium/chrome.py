ad1572a54cff312309ebeabb0c5edd9de770de40
Arun Kishore <50844460+rpakishore@users.noreply.github.com>
2023-12-04 22:49:25 -0800

    Update chrome.py

--- src/ak_selenium/chrome.py
+++ src/ak_selenium/chrome.py
@@ -1,9 +1,6 @@
 from helium import start_chrome
 from selenium import webdriver
-
-#from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-#from webdriver_manager.chrome import ChromeDriverManager
 
 import logging
 import os
