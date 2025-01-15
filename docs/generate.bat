1de6f8e49017243ef334b762bfe991b3e4bafba0
Arun Kishore <50844460+rpakishore@users.noreply.github.com>
2025-01-15 06:55:43 -0800

    Update generate.bat

--- docs/generate.bat
+++ docs/generate.bat
@@ -2,4 +2,4 @@ rem Change the current working directory to the directory of the batch script
 cd /d %~dp0
 cd ..
 
-uv run --dev pdoc --docformat google --math --mermaid template_python -o docs
+uv run --dev pdoc --docformat google --math --mermaid ak_selenium -o docs
